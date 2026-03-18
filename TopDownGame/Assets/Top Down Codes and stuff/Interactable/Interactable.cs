232cbe3cc6b1928a279ece5c4154d90bfb6627fb
dense123 <izzulhilmi@yahoo.com>
2026-03-18 11:20:23 +0800

    Imported outline asset

---
+++ TopDownGame/Assets/Top Down Codes and stuff/Interactable/Interactable.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Outline))]
+public class Interactable : MonoBehaviour
+{
+    [SerializeField] bool isInteractedWith;
+    [SerializeField] bool isHovering;
+    Outline outline;
+
+
+    protected virtual void Start()
+    {
+        outline = GetComponent<Outline>();
+    }
+
+    protected virtual void Update()
+    {
+        
+    }
+
+    protected void ActivateOutline()
+    {
+        outline.enabled = true;
+    }
+
+    protected void DeactivateOutline()
+    {
+        outline.enabled = false;
+    }
+}
